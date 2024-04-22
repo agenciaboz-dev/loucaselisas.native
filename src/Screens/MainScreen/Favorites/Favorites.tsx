097a48815646ae1aa2857a7c747fae3916095300
nandobfer <nandobfer@yahoo.com.br>
2024-04-21 22:13:11 -0300

    feat: started favorites

--- src/Screens/MainScreen/Favorites/Favorites.tsx
+++ src/Screens/MainScreen/Favorites/Favorites.tsx
@@ -1,16 +1,22 @@
 import { NavigationProp } from "@react-navigation/native"
-import React from "react"
+import React, { useState } from "react"
 import { View } from "react-native"
 import { Text } from "react-native-paper"
+import { ScreenTitle } from "../../../components/ScreenTItle"
+import { Lesson } from "../../../types/server/class/Course/Lesson"
 
 interface FavoritesProps {
     navigation: NavigationProp<any, any>
 }
 
 export const Favorites: React.FC<FavoritesProps> = ({ navigation }) => {
+    const [lessons, setLessons] = useState<Lesson[]>([])
+    const [filteredLessons, setFilteredLessons] = useState(lessons)
+    const [refreshing, setRefreshing] = useState(true)
+
     return (
-        <View style={{ flex: 1 }}>
-            <Text>favoritos</Text>
+        <View style={{ flex: 1, padding: 20 }}>
+            <ScreenTitle title="Seus Favoritos" hideBackArrow />
         </View>
     )
 }
